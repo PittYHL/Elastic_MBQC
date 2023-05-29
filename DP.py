58ecd9e31d5b1b5b3c6b363516926f4d776d5c06
PittYHL <yil392@pitt.edu>
2023-05-29 17:30:26 -0400

    need to finish place and update

--- DP.py
+++ DP.py
@@ -1,6 +1,10 @@
 import copy
 import networkx as nx
 from matplotlib import pyplot as plt
+
+la_win = 1
+keep = 3
+long = 3
 def DP(ori_map, qubits, rows):
     new_map = []
     for rows in ori_map:
@@ -12,8 +16,8 @@ def DP(ori_map, qubits, rows):
                 new_map[-1].append(2)
             else:
                 new_map[-1].append(0)
-    graph, nodes, C_len, W_len, first, last = gen_index(new_map)
-    place_core(graph, nodes, C_len, W_len, rows)
+    graph, nodes, W_len, first, last = gen_index(new_map)
+    place_core(graph, nodes, W_len, rows)
 
 def gen_index(map):
     cut = []
@@ -51,7 +55,7 @@ def gen_index(map):
         qubit = qubit + 1
         first.append(front) #length for the patterns in the front
         last.append(back) #length for the patterns in the back
-    nodes, C_len, W_len = gen_DAG(map,s_row)
+    nodes, W_len = gen_DAG(map,s_row)
     graph = nx.DiGraph()
     for node in nodes:
         for i in range(len(node) - 1):
@@ -59,7 +63,7 @@ def gen_index(map):
     nx.draw_networkx(graph)
     #order = list(nx.topological_sort(graph))
     #next = list(graph.successors('A.0'))
-    return graph, nodes, C_len, W_len, first, last
+    return graph, nodes, W_len, first, last
 
 def gen_DAG(map, s_row):
     indexes = []
@@ -78,7 +82,7 @@ def gen_DAG(map, s_row):
     W = 0
     A_loc = []
     B_loc = []
-    C_len = [] #none wire single row length
+    #C_len = [] #none wire single row length
     W_len = [] #wire single row length
     index = 0
     while index < len(indexes):
@@ -106,7 +110,7 @@ def gen_DAG(map, s_row):
     for i in range(len(s_row)):
         add = 1
         for j in range(len(s_row[i]) - 1):
-            if s_row[i][j + 1] -  s_row[i][j] > 1:
+            if s_row[i][j + 1] - s_row[i][j] > 1:
                 if map[i * 2][s_row[i][j] + 1] == 2:
                     node = 'W.' + str(W)
                     W = W + 1
@@ -115,14 +119,129 @@ def gen_DAG(map, s_row):
                     add = add + 1
                     W_len.append(s_row[i][j + 1] - s_row[i][j] - 1)
                 elif map[i * 2][s_row[i][j] + 1] != 2:
-                    node = 'C.' + str(C)
-                    C = C + 1
-                    loc = node_loc[i].index(s_row[i][j])
-                    nodes[i].insert(loc + add, node)
-                    add = add + 1
-                    C_len.append(s_row[i][j + 1] -  s_row[i][j] - 1)
-    return nodes, C_len, W_len
+                    for k in range(s_row[i][j + 1] - s_row[i][j] - 1):
+                        node = 'C.' + str(C)
+                        C = C + 1
+                        loc = node_loc[i].index(s_row[i][j])
+                        nodes[i].insert(loc + add, node)
+                        add = add + 1
+                    #C_len.append(s_row[i][j + 1] -  s_row[i][j] - 1)
+    return nodes, W_len
 
-def place_core(graph, nodes, C_len, W_len, rows):
+def place_core(graph, nodes, W_len, rows):
+    qubits = len(nodes)
     order = list(nx.topological_sort(graph))
+    placed = []
+    #place the first one
+    table = []
+    shape = []
+    chose = [] #for chosen patterns
+    current_row = 3
+    current_d = 0
+    for i in range(len(order)):
+        table.append([])
+        shape.append([])
+        chose.append([])
+    nodes_left = copy.deepcopy(order)
+    current = nodes_left.pop(0)
+    placed.append(current)
+    next = list(graph.successors(current))
+    gate, _ = current.split('.')
+    temp_shape = []
+    if gate == 'A':
+        dep = 1
+        temp_shape.append([1])
+        temp_shape.append([1])
+        temp_shape.append([1])
+    else:
+        dep = 2
+        temp_shape.append([1,1])
+        temp_shape.append([1,1])
+        temp_shape.append([1,1])
+    # for pat in next:
+    #     gate, _ = pat.split('.')
+    #     if gate == 'B':
+    #         LD = dep + 1
+    #     else:
+    #         LD = dep
+    table[0].append({'Parent':'NA', 'P':'NA', 'row':3, 'S':0, 'D':dep, 'Q':2, 'front':[[2,0], [0,0]]})
+    shape[0].append(temp_shape)
+    chose[0].append(0)
+    previous = current
+    while nodes_left != []:
+        #current version, the wires are added at the end
+        p_index = order.index(previous) # the index of the previous pattern
+        parent_node = chose[p_index]
+        if len(next) == 2: #first priorize two-qubit gate, than c
+            gate1, _ = next[0].split('.')
+            gate2, _ = next[0].split('.')
+            if gate1 == 'A' or gate1 == 'B':
+                current = next[0]
+                loc = 'u'
+            elif gate2 == 'A' or gate2 == 'B':
+                current = next[1]
+                loc = 'd'
+            elif gate1 == 'C':
+                current = next[0]
+                loc = 'u'
+            elif gate2 == 'C':
+                current = next[1]
+                loc = 'd'
+        else: # only has wire
+            current = next[0]
+            loc = check_loc(nodes, previous, current)
+        place_next(parent_node, loc, previous, current, table, shape, chose, nodes, p_index)
+        print('g')
+
+def check_loc(nodes, previous, current):
+    p_gate, _ = previous.split('.')
+    c_gate, _ = current.split('.')
+    if p_gate == 'A' or 'B':
+        qubit1 = 0
+        for i in range(len(nodes)):
+            if previous in nodes[i]:
+                qubit1 = i
+                break
+        if current in nodes[qubit1]:
+            loc = 'u'
+        else:
+            loc = 'd'
+    elif c_gate == 'A' or 'B':
+        qubit1 = 0
+        for i in range(len(nodes)):
+            if current in nodes[i]:
+                qubit1 = i
+                break
+        if previous in nodes[qubit1]:
+            loc = 'd'
+        else:
+            loc = 'u'
+    else:
+        loc = 'r'
+    return loc
+
+def place_next(parent_node, loc, previous, current, table, shape, chose, nodes, p_index):
+    c_gate, _ = current.split('.')
+    for index in parent_node:
+        c_shape = shape[p_index][index]
+        c_table = table[p_index][index]
+        if loc == 'd':
+            base = c_table['front'][1]
+        else:
+            base = c_table['front'][0]
+        if c_gate == 'C':
+            new_shape = place_C(c_shape, c_gate, base)
+        elif c_gate == 'A':
+            new_shape = place_A(c_shape, c_gate, base)
+        elif c_gate == 'B':
+            new_shape = place_B(c_shape, c_gate, base)
+
+def place_C(c_shape, c_gate, base): #place single node
+
+    print('g')
+
+def place_B(c_shape, c_gate, base): #place B node
+    print('g')
+
+def place_A(c_shape, c_gate, base): #place A node
     print('g')
